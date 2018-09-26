cbd46e69ea7dd0017b9a6548e703e96effcc2976
Lyken17 <lykensyu@gmail.com>
2018-09-26 00:37:58 -0400

    Migrating Tony's Mobilenet V2.

--- tools/trainval_net.py
+++ tools/trainval_net.py
@@ -20,6 +20,7 @@ import sys
 from nets.vgg16 import vgg16
 from nets.resnet_v1 import resnetv1
 from nets.mobilenet_v1 import mobilenetv1
+from nets.mobilenet_v2 import mobilenetv2
 
 def parse_args():
   """
@@ -130,6 +131,8 @@ if __name__ == '__main__':
     net = resnetv1(num_layers=152)
   elif args.net == 'mobile':
     net = mobilenetv1()
+  elif args.net == 'mnet':
+    net = mobilenetv2()
   else:
     raise NotImplementedError
     
